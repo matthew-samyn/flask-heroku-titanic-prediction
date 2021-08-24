4db41da308a7f41f933c411f1ab16ea757133ac2
matthew-samyn <matthew.samyn1@gmail.com>
2021-08-24 14:32:54 +0200

    Updating progress - Flask

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 import pickle
 
 # load model
-model = pickle.load(open('model/model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 # app
 app = Flask(__name__)
